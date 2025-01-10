05b94e7242cc41fbaa799fba9b81aeeca7aaf356
Marco Dugatto <dughi.boogie@gmail.com>
2025-01-10 13:59:19 +0100

    Moved interaction logic to player node

--- Scripts/InteractionSystem/NPC.gd
+++ Scripts/InteractionSystem/NPC.gd
@@ -1,12 +1,2 @@
 class_name NPC
 extends Interactable
-
-
-func _on_interaction_area_entered(area: Area3D) -> void:
-	if area.name == "PlayerInteractionArea":
-		EventBus.emit_signal("interaction_event", {"type": "show_prompt", "interaction_message": "Talk to " + interactable_name})
-
-
-func _on_interaction_area_exited(area: Area3D) -> void:
-	if area.name == "PlayerInteractionArea":
-		EventBus.emit_signal("interaction_event", {"type": "hide_prompt"})
--- Scripts/Player/PlayerInteraction.gd
+++ Scripts/Player/PlayerInteraction.gd
@@ -8,9 +8,21 @@ enum InteractionTypes {
 }
 
 
-var current_interaction_entity
-
-
 func _on_body_entered(body: Node3D) -> void:
-	if body.name == "NPCStaticBody3D":
-		print("body found!")
+	if body.get_parent_node_3d() is NPC:
+		var npc = body.get_parent_node_3d() as NPC
+		EventBus.emit_signal("interaction_event", {"type": "show_prompt", "interaction_message": "Talk to " + npc.interactable_name})
+
+
+func _on_body_exited(body: Node3D) -> void:
+	if not has_overlapping_bodies():
+		EventBus.emit_signal("interaction_event", {"type": "hide_prompt"})
+		return
+	
+	var overlapping_bodies = get_overlapping_bodies()
+	
+	for overlapping_body in overlapping_bodies:
+		if body.get_parent_node_3d() is NPC:
+			var npc = body.get_parent_node_3d() as NPC
+			EventBus.emit_signal("interaction_event", {"type": "show_prompt", "interaction_message": "Talk to " + npc.interactable_name})
+			return
